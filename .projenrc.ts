2bac2a5d9f0165f88975189240d50b4e8b8a7d1e
Antonio Márquez Pérez <antonio.marquez@jumptothecloud.tech>
2023-10-23 14:02:11 +0200

    chore(projen): repository basic configuration

--- .projenrc.ts
+++ .projenrc.ts
@@ -1,17 +1,34 @@
 import { awscdk } from 'projen';
+import { TrailingComma } from 'projen/lib/javascript';
 const project = new awscdk.AwsCdkConstructLibrary({
   author: 'Antonio Márquez Pérez',
   authorAddress: 'antonio.marquez@jumptothecloud.tech',
-  cdkVersion: '2.1.0',
+  cdkVersion: '2.91.0',
   defaultReleaseBranch: 'main',
   jsiiVersion: '~5.0.0',
-  name: 'aws-codestarconnection',
+  name: '@jttc/aws-codestarconnection',
   projenrcTs: true,
-  repositoryUrl: 'https://github.com/antonio.marquez/aws-codestarconnection.git',
-
-  // deps: [],                /* Runtime dependencies of this module. */
-  // description: undefined,  /* The description is just a string that helps people understand the purpose of the package. */
-  // devDeps: [],             /* Build dependencies for this module. */
-  // packageName: undefined,  /* The "name" in package.json. */
+  constructsVersion: '10.3.0',
+  repositoryUrl: 'https://github.com/JumpToTheCloud/aws-codestarconnection',
+  prettier: true,
+  prettierOptions: {
+    settings: {
+      trailingComma: TrailingComma.ES5,
+      singleQuote: true,
+      bracketSpacing: true,
+      semi: true,
+    },
+  },
+  jestOptions: {
+    jestConfig: {
+      verbose: true,
+    },
+  },
+  deps: [] /* Runtime dependencies of this module. */,
+  peerDeps: [] /* Peer dependencies of this module. */,
+  description:
+    'AWS CDK L2 construct for aws code star connection' /* The description is just a string that helps people understand the purpose of the package. */,
+  devDeps: [] /* Build dependencies for this module. */,
+  packageName: '@jttc/aws-codestarconnection' /* The "name" in package.json. */,
 });
 project.synth();
---
+++ API.md
@@ -0,0 +1,40 @@
+# replace this
+# API Reference <a name="API Reference" id="api-reference"></a>
+
+
+
+## Classes <a name="Classes" id="Classes"></a>
+
+### Hello <a name="Hello" id="@jttc/aws-codestarconnection.Hello"></a>
+
+#### Initializers <a name="Initializers" id="@jttc/aws-codestarconnection.Hello.Initializer"></a>
+
+```typescript
+import { Hello } from '@jttc/aws-codestarconnection'
+
+new Hello()
+```
+
+| **Name** | **Type** | **Description** |
+| --- | --- | --- |
+
+---
+
+#### Methods <a name="Methods" id="Methods"></a>
+
+| **Name** | **Description** |
+| --- | --- |
+| <code><a href="#@jttc/aws-codestarconnection.Hello.sayHello">sayHello</a></code> | *No description.* |
+
+---
+
+##### `sayHello` <a name="sayHello" id="@jttc/aws-codestarconnection.Hello.sayHello"></a>
+
+```typescript
+public sayHello(): string
+```
+
+
+
+
+
