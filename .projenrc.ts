1410060fb0e82131faa52bd456cb8a4918924839
Antonio Márquez Pérez <antonio.marquez@jumptothecloud.tech>
2023-10-26 18:55:56 +0200

    chore(docs): remove doble quotes when we adde the git user configuration
    there was an unexpectd doble quotes

--- .projenrc.ts
+++ .projenrc.ts
@@ -113,8 +113,8 @@ deployDocs?.addJob('deploy-docs', {
     {
       name: 'Setup docs deploy',
       run: [
-        'git config user.name "github-actions',
-        'git config user.email "github-actions@github.com',
+        'git config user.name github-actions',
+        'git config user.email github-actions@github.com',
       ].join('\n'),
     },
     {
